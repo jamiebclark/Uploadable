48a346e1f93e8e2ead0280779364984aed48bfb1
Jamie Desktop <jamiebclark@gmail.com>
2016-02-28 14:33:37 -0500

    Added full URL path source to FieldUpload image info

--- Model/Behavior/FieldUploadBehavior.php
+++ Model/Behavior/FieldUploadBehavior.php
@@ -675,6 +675,7 @@ class FieldUploadBehavior extends ModelBehavior {
 			if (!empty($this->_urlBase)) {
 				$src = $this->_urlBase . $src;
 			}
+			$src = Router::url($src, true);
 		}
 		if (is_file($path)) {
 			$info = getimagesize($path);
